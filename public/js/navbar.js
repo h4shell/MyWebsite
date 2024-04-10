1ec75929753e0dd41dacd8420079345465e6e9a7
Lorenzo Fornara <fw1987@gmail.com>
2024-04-10 14:37:42 +0200

    update navbab hamburger animation

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,6 +2,12 @@ const nav = document.querySelector("nav")
 const hamburger = nav.querySelector(".hamburger");
 const containerMenu = nav.querySelector(".container-menu")
 
+
 hamburger.addEventListener("click", () => {
+    const spans = hamburger.querySelectorAll("span")
     containerMenu.classList.toggle("active")
+    spans[1].classList.toggle("d-none")
+    spans[0].classList.toggle("rotate1")
+    spans[2].classList.toggle("rotate2")
+
 })
