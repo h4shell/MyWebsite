03354526320860c4580eb8d3f15693e959899ca3
h4shell <60841743+h4shell@users.noreply.github.com>
2024-04-12 02:07:22 +0200

    Update contact.js

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 
 const sendButton = form.querySelector(".btn");
-const url = "/contact.php";
+let url = "/contact.php";
 
 sendButton.addEventListener("click", (event) => {
   event.preventDefault();
