9ee9a1e37c3fc679a1cdae158147644f0633be4a
Lorenzo Fornara <fw1987@gmail.com>
2024-04-18 11:37:27 +0200

    update contact.php

--- contact.php
+++ contact.php
@@ -34,18 +34,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $mail = new PHPMailer(true);
 
 
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->isSMTP();                                 //Send using SMTP
+    $mail->Host = 'smtp.gmail.com';                  //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                          //Enable SMTP authentication
     $mail->Username = $USERNAME;                     //SMTP username
-    $mail->Password = $PASSWORD;                               //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Password = $PASSWORD;                     //SMTP password
+    $mail->SMTPSecure = 'ssl';                       //Enable implicit TLS encryption
     $mail->Port = 465;
 
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('fw1987@gmail.com', 'Joe User');     //Add a recipient
+    $mail->setFrom('fw1987@gmail.com', 'Email h4sh.it da:' . $email);
+    $mail->addAddress('fw1987@gmail.com', 'Lorenzo Fornara');     //Add a recipient
 
 
     //Content
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $mail->Subject = 'Email dal sito internet';
     $mail->Encoding = 'base64';
 
-    $mail->Body = $name . ' ' . $email . ' ' . $message;
+    $mail->Body = $name . '<br><br>' . $email . '<br><br>' . $message;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 
