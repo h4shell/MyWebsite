6232817aa1179ea0430a19a7e992de549348936e
Lorenzo Fornara <fw1987@gmail.com>
2024-09-30 10:19:51 +0000

    fixed Menu

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector("nav")
 const hamburger = nav.querySelector(".hamburger");
 const containerMenu = nav.querySelector(".container-menu")
-
+const liMenu = containerMenu.querySelectorAll("li")
 
 hamburger.addEventListener("click", () => {
     const spans = hamburger.querySelectorAll("span")
@@ -9,5 +9,15 @@ hamburger.addEventListener("click", () => {
     spans[1].classList.toggle("d-none")
     spans[0].classList.toggle("rotate1")
     spans[2].classList.toggle("rotate2")
+})
 
+liMenu.forEach((li) => {
+    li.addEventListener("click", () => {
+        const spans = hamburger.querySelectorAll("span")
+        containerMenu.classList.remove("active")
+        spans[1].classList.remove("d-none")
+        spans[0].classList.remove("rotate1")
+        spans[2].classList.remove("rotate2")
+
+    })
 })
